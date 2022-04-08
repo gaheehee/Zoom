74de03665869b6b04736f228ecedcccaefbf3e89
Jeong Ga Hee <jghgahee12@gmail.com>
2022-04-08 14:50:09 +0900

    prepare for real-time chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,18 @@
 //app.js의 socket은 서버로의 연결을 뜻함
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected to Server ❌");
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -22,7 +22,12 @@ const wss = new WebSocket.Server({ server });   //WebSocket 서버 생성
 
 //server.js의 socket은 연결된 browser를 뜻함
 wss.on("connection", (socket) => {
-    console.log(socket);
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    socket.on("message", message => {
+        console.log(message.toString('utf8'));
+    });
+    socket.send("hello!!!!");
 });
 
 server.listen(3000, handleListen);
